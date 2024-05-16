5006191149a4b696f1f0c67509910b74993f58a1
profdf <165510871+profdf@users.noreply.github.com>
2024-05-15 22:13:12 -0300

    adicionado codigo base

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <Arduino.h>
-#include "libs/SerialMonitor.h"
+#include <LiquidCrystal.h>
+
+// BACKLIGHT (VO) -> GND
+const int rs = 19, en = 23, d4 = 18, d5 = 17, d6 = 16, d7 = 15;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+uint8_t count = 0;
 
 void setup() {
 
-  Serial.begin(115200);
-  print(Serial, "Hello World!");
+  lcd.begin(16, 2);
+  lcd.print("Iniciando...");
+
+  delay(5000);
 
 }
 
 void loop() {
 
-  char readed = read(Serial);
-
-  if(readed){
-    print(Serial, readed);
-  }
-
-  delay(100);
+  lcd.clear();
+  lcd.print(count++);
+  delay(1000);
 
 }
