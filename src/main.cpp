cca1009618fc1b6237136b55d9541ba3deafa004
profdf <165510871+profdf@users.noreply.github.com>
2024-06-12 22:16:54 -0300

    Adicionado código da aula

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,130 @@
-#include <Arduino.h>
-#include <LiquidCrystal.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 #include "DHT.h"
 
-const int rs = 19, en = 23, d4 = 18, d5 = 17, d6 = 16, d7 = 15;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+void readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file){
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)){
+      Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (fs.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+  Serial.printf("Deleting file: %s\n", path);
+  if(fs.remove(path)){
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+  File file = fs.open(path);
+  static uint8_t buf[512];
+  size_t len = 0;
+  uint32_t start = millis();
+  uint32_t end = start;
+  if(file){
+    len = file.size();
+    size_t flen = len;
+    start = millis();
+    while(len){
+      size_t toRead = len;
+      if(toRead > 512){
+        toRead = 512;
+      }
+      file.read(buf, toRead);
+      len -= toRead;
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes read for %u ms\n", flen, end);
+    file.close();
+  } else {
+    Serial.println("Failed to open file for reading");
+  }
+
+
+  file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  size_t i;
+  start = millis();
+  for(i=0; i<2048; i++){
+    file.write(buf, 512);
+  }
+  end = millis() - start;
+  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+  file.close();
+}
 
 #define DHTPIN 2
 #define DHTTYPE DHT11   
 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-
+void setup(){
+  
   Serial.begin(115200);
-  lcd.begin(16, 2);
-  lcd.print("Iniciando...");
-
-  delay(1000);
-
-  lcd.clear();
-
-  Serial.println(F("DHTxx test!"));
-
   dht.begin();
+
+  if(!SD.begin(5)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.println("+");  
+  readFile(SD, "/readings.csv");
+  Serial.println("+");
+  delay(2000);
+
 }
 
-void loop() {
-  delay(2000);
+void loop(){
+
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   float f = dht.readTemperature(true);
@@ -35,18 +133,20 @@ void loop() {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
+  
+  // Formata cada leitura para CSV
+  String reading = "";
+  reading.concat(String(h));
+  reading.concat(";");
+  reading.concat(String(t));
+  reading.concat("\n");
+  // ---
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.println(F("°F"));
+  Serial.print(reading);
 
-  lcd.setCursor(0, 0);
-  lcd.printf("UMIDADE: %.1f %", h);
-  lcd.setCursor(0, 1);
-  lcd.printf("TEMPERATURA:%.1fC", t);
+  appendFile(SD, "/readings.csv", reading.c_str());
+
+  delay(2000);
+
 
 }
