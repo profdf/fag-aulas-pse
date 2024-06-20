f1a0ed79e34e29f8389facd9ee0c92aadae54425
profdf <165510871+profdf@users.noreply.github.com>
2024-06-19 21:59:27 -0300

    arquivo final da aula

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
-
+#include <DHT.h>
 #include <Firebase_ESP_Client.h>
 
 //Provide the token generation process info.
@@ -9,14 +9,14 @@
 #include "addons/RTDBHelper.h"
 
 // Insert your network credentials
-#define WIFI_SSID "Mi 10T"
-#define WIFI_PASSWORD "12345678"
+#define WIFI_SSID "NOME_REDE"
+#define WIFI_PASSWORD "SENHA"
 
 // Insert Firebase project API Key
-#define API_KEY "AIzaSyAdwjUJ169K8CSPgEXy_vV5KShwpB1mMP8"
+#define API_KEY "AIzaSyAJ6xRK6Tt_Z_hYubzDPbPLpzjQCbsHa_g"
 
 // Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://aula-pse-default-rtdb.firebaseio.com/" 
+#define DATABASE_URL "https://fag-pse-teste-default-rtdb.firebaseio.com/" 
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -25,11 +25,45 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
-int count = 0;
 bool signupOK = false;
 
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE    DHT11     // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+float readDHTTemperature() {
+
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  else {
+    Serial.println(t);
+    return t;
+  }
+
+}
+
+float readDHTHumidity() {
+
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  else {
+    Serial.println(h);
+    return h;
+  }
+
+}
 void setup(){
+
   Serial.begin(115200);
+
+  dht.begin();
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
@@ -61,13 +95,14 @@ void setup(){
   
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
 }
 
 void loop(){
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
-    // Write an Int number on the database path test/int
-    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+
+    if (Firebase.RTDB.setFloat(&fbdo, "leituras/temperatura", readDHTTemperature())){
       Serial.println("PASSED");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
@@ -76,10 +111,8 @@ void loop(){
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
-    count++;
     
-    // Write an Float number on the database path test/float
-    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+    if (Firebase.RTDB.setFloat(&fbdo, "leituras/umidade", readDHTHumidity())){
       Serial.println("PASSED");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
@@ -89,4 +122,5 @@ void loop(){
       Serial.println("REASON: " + fbdo.errorReason());
     }
   }
+
 }
